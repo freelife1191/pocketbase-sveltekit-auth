02d8904bc9485ae956a9696c995fba4ac6eb9a05
권민승 <freelife1191.good@gmail.com>
2023-10-30 16:35:29 +0900

    로그인 정보 이용해서 사이트 꾸미기

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,11 +1,23 @@
+<script>
+  export let data;
+</script>
+
 <div class="container">
     <nav>
       <ul>
-        <li><strong>Brand</strong></li>
+        <li><a href="/" style="color: orange; font-weight: bold; font-size: 2rem">Brand</a></li>
       </ul>
       <ul>
-        <li><a href="/login">로그인</a></li>
-        <li><a href="/register">회원가입</a></li>
+        {#if data?.profile}
+          <li>
+            <form action="/logout" method="POST">
+              <button>로그아웃</button>
+            </form>
+          </li>
+        {:else}
+          <li><a href="/login">로그인</a></li>
+          <li><a href="/register">회원가입</a></li>
+        {/if}
       </ul>
     </nav>
     <slot />
